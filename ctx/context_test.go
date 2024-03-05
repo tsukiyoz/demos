60e5f54fe98d8fb64d9f89d72736e52b54ed1a48
tsukiyo <tsukiyo6@163.com>
2024-03-05 23:31:51 +0800

    feat: add father son context cancel usage

--- ctx/context_test.go
+++ ctx/context_test.go
@@ -2,6 +2,8 @@ package ctx
 
 import (
 	"context"
+	"errors"
+	"sync"
 	"testing"
 	"time"
 )
@@ -48,4 +50,87 @@ func TestCancel(t *testing.T) {
 	// control context to quit
 	time.Sleep(3 * time.Second)
 	cancel()
+}
+
+func TestContextErr(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	time.Sleep(time.Second * 4)
+	cancel()
+
+	switch err := ctx.Err(); {
+	case errors.Is(err, context.Canceled):
+		t.Logf("cancelled!\n")
+	case errors.Is(err, context.DeadlineExceeded):
+		t.Logf("dealine exceeded!\n")
+	}
+}
+
+func TestContextFatherCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	subCtx, _ := context.WithCancel(ctx)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second * 3)
+		t.Logf("father ctx cancelled!\n")
+		cancel()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			switch err := subCtx.Err(); {
+			case errors.Is(err, context.Canceled):
+				t.Logf("son ctx has been cancelled!\n")
+				return
+			case errors.Is(err, context.DeadlineExceeded):
+				t.Logf("son context not cancelled until timeout!\n")
+				return
+			default:
+				t.Logf("son ctx waiting for cancel...\n")
+				time.Sleep(time.Second)
+			}
+		}
+	}()
+
+	wg.Wait()
+}
+
+func TestContextSonCancel(t *testing.T) {
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*7)
+	_, sonCancel := context.WithCancel(ctx)
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		time.Sleep(time.Second * 3)
+		t.Logf("son ctx cancelled!\n")
+		sonCancel()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			switch err := ctx.Err(); {
+			case errors.Is(err, context.Canceled):
+				t.Logf("father context has been cancelled!\n")
+				return
+			case errors.Is(err, context.DeadlineExceeded):
+				t.Logf("father context not cancelled until timeout!\n")
+				return
+			default:
+				time.Sleep(time.Second)
+				t.Logf("father ctx waiting for cancel...\n")
+			}
+		}
+	}()
+
+	wg.Wait()
 }
