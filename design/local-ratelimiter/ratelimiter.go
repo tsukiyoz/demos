6a243409e2e95129b74beb434ab5ca97a3a1720a
tsukiyo <tsukiyo6@163.com>
2025-04-08 18:12:28 +0800

    docs: local_ratelimiter add documents

--- design/local-ratelimiter/ratelimiter.go
+++ design/local-ratelimiter/ratelimiter.go
@@ -7,6 +7,14 @@ import (
 	"golang.org/x/sys/cpu"
 )
 
+// RateLimiter is a simple rate limiter that allows a certain number of requests per second.
+// It uses atomic operations to ensure thread safety and avoid contention.
+// The rate limiter is designed to be used in high-performance scenarios where low latency is critical.
+// It uses a 64-bit integer to store the state, where the upper 40 bits represent the timestamp
+// and the lower 24 bits represent the number of requests made in the current second.
+// The rate limiter allows up to 0xFFFFFF requests per second (approximately 16 million).
+// The timestamp is stored in the upper 40 bits of the state, which allows for a maximum of
+// 2^40 seconds (approximately 1.1 trillion years) before the timestamp wraps around.
 type RateLimiter struct {
 	_     cpu.CacheLinePad
 	state uint64
@@ -37,7 +45,6 @@ func (limiter *RateLimiter) Take(now uint64) bool {
 		ts := state >> 24 // 右移24位,低24位表示的是qps
 		if ts < now {
 			newState := now<<24 + 1 // 左移24位
-
 			if atomic.CompareAndSwapUint64(&limiter.state, state, newState) {
 				return true
 			}
